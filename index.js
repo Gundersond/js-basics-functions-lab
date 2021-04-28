44e7b9effcda96f92463b292889984cf52919465
Gundersond <dalton.gunderson@hotmail.com>
2021-04-27 22:16:07 -0500

    commit tree 6b7f292d3855757054f750d2d62ef9263bbbffbb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const distance = Math.abs(42 - block);
+    return distance;
+}
+
+function distanceFromHqInFeet(block) {
+    const distance = ((distanceFromHqInBlocks(block)) * 264);
+    return distance; 
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    const distanceTravelled = (Math.abs(startingBlock - endingBlock) * 264);
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+    const distance = distanceTravelledInFeet(startingBlock, endingBlock);
+    let price;
+    if (distance <= 400) {
+        price = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        price = ((distance - 400) * 0.02);
+    } else if (distance > 2000 && distance <= 2500) {
+        price = 25;
+    } else if (distance > 2500) {
+        price = "cannot travel that far";
+    }
+    return price;
+}
